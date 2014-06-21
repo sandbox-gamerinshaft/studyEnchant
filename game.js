dcebad0213446ca6eb1922fc7df980ecf3989104
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-06-21 20:57:15 +0900

    update

--- game.js
+++ game.js
@@ -3,3 +3,27 @@ DISPLAY_HEIGHT = 720;
 
 enchant();
 var core = {};
+window.onload = function(){
+  core = new Core(DISPLAY_WIDTH,DISPLAY_HEIGHT);
+  var gazou = [];
+  gazou.push("../images/unit_catch/src/game.png");
+  gazou.push("../images/unit_catch/src/game_over1.png");
+  gazou.push("../images/unit_catch/src/game_over2.png");
+  gazou.push("../images/unit_catch/src/mol.png");
+  gazou.push("../images/unit_catch/src/print.png");
+  gazou.push("../images/unit_catch/src/title.png");
+  core.preload(gazou);
+  core.onload = function(){
+    var gameGamen = new Scene();
+    core.pushScene(gameGamen);
+    var mol = new Sprite(151,169);
+    mol.image = core.assets["../images/unit_catch/src/mol.png"];
+    mol.moveTo(300, 500);
+    mol.addEventListener("touchmove",function(e){
+      this.x = e.x - this.width/2;
+      // this.y = e.y - this.height/2;
+    });
+    gameGamen.addChild(mol);
+  };
+  core.start();
+};
