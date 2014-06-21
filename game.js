f9925bcfbb2d2332089c005d846d71fac12320d8
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-06-21 21:10:06 +0900

    molの向きが入れ替わるようにした

--- game.js
+++ game.js
@@ -20,6 +20,11 @@ window.onload = function(){
     mol.image = core.assets["../images/unit_catch/src/mol.png"];
     mol.moveTo(300, 500);
     mol.addEventListener("touchmove",function(e){
+      if(e.x - this.width/2 > this.x){
+        this.frame = 0;
+      }else{
+        this.frame = 1;
+      }
       this.x = e.x - this.width/2;
       // this.y = e.y - this.height/2;
     });
