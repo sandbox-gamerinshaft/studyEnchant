515e52798080993d96b80f18b6481eb362aece6d
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-06-24 15:10:26 +0900

    五回目の練習

---
+++ practice5.js
@@ -0,0 +1,21 @@
+D_W = 1280;
+D_H = 720;
+
+enchant();
+var core = {};
+
+window.onload = function(){
+  core = new Core(D_W, D_H);
+  var gazou = [];
+  gazou.push("");
+  gazou.push("");
+  gazou.push("");
+  gazou.push("");
+  gazou.push("");
+  core.preload(gazou)
+  window.core = function(){
+    gameGamen = new Scene();
+    core.pushScene(gameGamen);
+
+  };
+};
